62ed55d46d3b14a807218a0088bea4127ee054ab
5ha2s6 <tech@5ha2s6.com>
2025-10-16 10:15:40 +0100

    Update DeserializerWiiicoin to inherit from DeserializerSegWit

--- src/electrumx/lib/tx_wiiicoin.py
+++ src/electrumx/lib/tx_wiiicoin.py
@@ -1,7 +1,7 @@
 # electrumx/lib/tx_wiiicoin.py
-from electrumx.lib.tx import Deserializer  # Use DeserializerSegWit if Wiiicoin has SegWit
+from electrumx.lib.tx import DeserializerSegWit  # Use DeserializerSegWit if Wiiicoin has SegWit
 
-class DeserializerWiiicoin(Deserializer):
+class DeserializerWiiicoin(DeserializerSegWit):
     """
     Minimal deserializer for Wiiicoin assuming Bitcoin-like tx format.
     If Wiiicoin differs (e.g., extra fields), override the parse_* methods here.
